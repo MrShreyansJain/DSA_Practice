a6d68f64e730dbcd6f801deddef8658f1bad2128
ShreyansJain24 <jainshreyans251@gmail.com>
2025-03-29 06:47:00 +0530

    commit for code changes in Reccursion code

---
+++ src/Recursion/Easy/SumTriangle_GFG.java
@@ -0,0 +1,21 @@
+package src.Recursion.Easy;
+
+import java.util.Arrays;
+
+public class SumTriangle_GFG {
+    public static void main(String[] args) {
+       int[] array= {1,2,3,4,5};
+        //System.out.println(getReducesArray(array,0));
+    }
+
+    private static void printTriangle(int[] array){
+
+
+
+        System.out.println(Arrays.toString(array));
+    }
+
+//    private static int[] getReducesArray(int[] temp,int[] array,int index){
+//
+//    }
+}
